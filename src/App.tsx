6b4282f6cb029e18e741676aec647302592697ec
Bruno Panerai Velloso <bpv.ifsc@gmail.com>
2023-05-26 11:14:25 -0300

    Criação da funcionalidade básica da calculadora

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Calculadora from './componentes/Calculadora';
 
 function App() {
+
+  const [num1, setNum1] = useState(0)
+  const [num2, setNum2] = useState(0)
+  const [resultado, setResultado] = useState(0)
+  const [operacao, setOperacao] = useState('+')
+
+  useEffect( ()=>{
+    if(operacao==='+'){
+      setResultado(num1 + num2)
+    }else if(operacao==='-'){
+      setResultado(num1 - num2)
+    }else if(operacao==='*'){
+      setResultado(num1 * num2)
+    }else if(operacao==='/'){
+      setResultado(num1 / num2)
+    }
   
+  } , [num1, num2, operacao])
+
   return (
-    <Calculadora />
+    <div>
+      <input type="number" value={num1} 
+             onChange={ evento => setNum1(Number(evento.target.value)) }/>
+      <div>
+        <button className={operacao==='+' ? 'operacao_selecionada' : ''  } 
+                onClick={ evento => setOperacao('+') }>+</button>
+
+        <button className={operacao==='-' ? 'operacao_selecionada' : ''  } 
+                onClick={ evento => setOperacao('-') }>-</button>
+
+        <button className={operacao==='*' ? 'operacao_selecionada' : ''  } 
+                onClick={ evento => setOperacao('*') }>*</button>
+
+        <button className={operacao==='/' ? 'operacao_selecionada' : ''  } 
+                onClick={ evento => setOperacao('/') }>/</button>
+      </div>
+      <input type="number" value={num2}
+            onChange={ evento => setNum2(Number(evento.target.value)) }/>
+      <hr/>
+      <input type="number" value={resultado}/>
+    </div>
   );
 }
 
--- src/componentes/Resposta/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Resposta({resultado}:{resultado:number}) {
-
-    return(
-        <div>
-            <input type="number" value={resultado} disabled/>
-        </div>
-    )
-    
-}
